17e7dd1d4a197158dedb758649e9700818054755
spark <sunha@drection.com>
2024-10-17 21:50:40 +0900

    lesson icons

--- app/components/lesson-icon.tsx
+++ app/components/lesson-icon.tsx
@@ -1,4 +1,8 @@
 export const LessonIcon = ({ lesson }: { lesson: number }) => {
   if (lesson === 1) return <img src="/ui/lesson1.png" />;
+  else if (lesson === 2) return <img src="/ui/lesson2.png" />;
+  else if (lesson === 3) return <img src="/ui/lesson3.png" />;
+  else if (lesson === 4) return <img src="/ui/lesson4.png" />;
+  else if (lesson === 5) return <img src="/ui/lesson5.png" />;
   return null;
 };
