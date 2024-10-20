04b3f6076c83af48185f11667c64b5de0c644658
Sunha <sunha@drection.com>
2024-10-21 02:08:07 +0900

    07 update

--- app/(lessons)/lesson3/(7 intro)/07_058_A3/page.tsx
+++ app/(lessons)/lesson3/(7 intro)/07_058_A3/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import ReactPlayer from "react-player";
+import BACKGROUND1 from "@/app/bgpng_temp/intro/heyhey.com_@ybm@_@MDCH@_main_main.html_lessonNum=3&subNum=2&pageNum=1_2.png"
 
 export default function Page() {
   return (
@@ -12,6 +13,7 @@ export default function Page() {
         muted
         playing
       />
+      <img src={BACKGROUND1.src} className="absolute left-0 top-0 w-[1760px] opacity-25 pointer-events-none" />
     </>
   );
 }
