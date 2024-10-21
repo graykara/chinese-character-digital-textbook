3dad11c725ad6a46bc59694b4db40147520f12e9
전윤민 <cordelia273@naver.com>
2024-10-21 17:13:38 +0900

    feat: 5단원 정리하기

--- app/(lessons)/lesson5/(16 outro)/(1 정리하기)/16_132_I1/page.tsx
+++ app/(lessons)/lesson5/(16 outro)/(1 정리하기)/16_132_I1/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { SummaryPageTemplate } from "@/app/pages/summary-page-template";
-import IMAGE from "./image.png";
+import IMAGE1 from "./image1.png";
 import IMAGE2 from "./image2.png";
+import IMAGE3 from "./image3.png";
+import IMAGE4 from "./image4.png";
 import { useState } from "react";
 import { StepContainer } from "@/app/components/step-container";
 import BACKGROUND1 from "@/app/bgpng_temp/6/중등한문_한자를 알면 쉬운 교과서 속 어휘23.png";
@@ -17,9 +19,11 @@ export default function Page() {
         <div className="w-full h-full">
           {step === 1 && <Step1 />}
           {step === 2 && <Step2 />}
+          {step === 3 && <Step3 />}
+          {step === 4 && <Step4 />}
         </div>
       </SummaryPageTemplate>
-      <StepContainer maxStep={2} step={step} onStepChange={setStep} />
+      <StepContainer maxStep={4} step={step} onStepChange={setStep} />
       {/* <img src={BACKGROUND2.src} className="debug absolute left-0 top-0 opacity-25 pointer-events-none" /> */}
     </>
   );
@@ -30,18 +34,18 @@ const Step1 = () => {
     <div className="relative pl-[90px]">
       <div className="flex items-center gap-1">
         <div className="animate__animated animate__flipInX absolute -left-11 -top-1 w-[60px] h-[60px] rounded-full bg-[#e64532] flex justify-center items-center">
-          <span className="font-cookierun text-white -ml-1">11</span>
+          <span className="font-cookierun text-white -ml-1">13</span>
         </div>
         <div className="relative -ml-14 -mt-2">
           <span className="font-cookierun text-[#74210d]">
-            나에게 힘이 되는 글
+            귀에 대고 말한 까닭
           </span>
         </div>
       </div>
 
-      <div className="-ml-10 -mt-10">
+      <div className="-ml-10 mt-10">
         <img
-          src={IMAGE.src}
+          src={IMAGE1.src}
           className="animate__animated animate__fadeIn relative top-2"
         />
       </div>
@@ -52,18 +56,65 @@ const Step1 = () => {
 const Step2 = () => {
   return (
     <div className="relative pl-[90px]">
-      <div className="flex items-center gap-5">
-        <div className="animate__animated animate__flipInX absolute -left-11 -top-1 w-[60px] h-[60px]  rounded-full bg-[#73210d] flex justify-center items-center">
-          <span className="font-cookierun text-white">2</span>
+      <div className="flex items-center gap-1">
+        <div className="animate__animated animate__flipInX absolute -left-11 -top-1 w-[60px] h-[60px] rounded-full bg-[#e64532] flex justify-center items-center">
+          <span className="font-cookierun text-white -ml-1">14</span>
         </div>
         <div className="relative -ml-14 -mt-2">
-          <span className="font-cookierun">단어의 짜임</span>
+          <span className="font-cookierun text-[#74210d]">사람만 귀한가요</span>
         </div>
       </div>
-      <div className="-mt-5 -ml-14">
+
+      <div className="-ml-10 mt-10">
         <img
           src={IMAGE2.src}
-          className="animate__animated animate__fadeIn relative"
+          className="animate__animated animate__fadeIn relative top-2"
+        />
+      </div>
+    </div>
+  );
+};
+
+const Step3 = () => {
+  return (
+    <div className="relative pl-[90px]">
+      <div className="flex items-center gap-1">
+        <div className="animate__animated animate__flipInX absolute -left-11 -top-1 w-[60px] h-[60px] rounded-full bg-[#e64532] flex justify-center items-center">
+          <span className="font-cookierun text-white -ml-1">15</span>
+        </div>
+        <div className="relative -ml-14 -mt-2">
+          <span className="font-cookierun text-[#74210d]">돌에 꽂힌 화살</span>
+        </div>
+      </div>
+
+      <div className="-ml-10 mt-10">
+        <img
+          src={IMAGE3.src}
+          className="animate__animated animate__fadeIn relative top-2"
+        />
+      </div>
+    </div>
+  );
+};
+
+const Step4 = () => {
+  return (
+    <div className="relative pl-[90px]">
+      <div className="flex items-center gap-1">
+        <div className="animate__animated animate__flipInX absolute -left-11 -top-1 w-[60px] h-[60px] rounded-full bg-[#e64532] flex justify-center items-center">
+          <span className="font-cookierun text-white -ml-1">16</span>
+        </div>
+        <div className="relative -ml-14 -mt-2">
+          <span className="font-cookierun text-[#74210d]">
+            제주 거상 김만덕
+          </span>
+        </div>
+      </div>
+
+      <div className="-ml-10 mt-10">
+        <img
+          src={IMAGE4.src}
+          className="animate__animated animate__fadeIn relative top-2"
         />
       </div>
     </div>
--- app/(lessons)/lesson5/(16 outro)/(활동하기)/16_133_J1/page.tsx
+++ app/(lessons)/lesson5/(16 outro)/(활동하기)/16_133_J1/page.tsx
@@ -1,24 +1,22 @@
 "use client";
 
-import IMAGE1 from "./image1.png";
-import IMAGE2 from "./image2.png";
+import IMAGE from "./image.png";
 import { StepContainer } from "@/app/components/step-container";
 import { useState } from "react";
 import { ActivityPageTemplate } from "@/app/pages/activity-page-template";
 import { ExampleAnswerButton } from "@/app/components/buttons/example-answer-button";
 import BACKGROUND1 from "@/app/bgpng_temp/6/중등한문_한자를 알면 쉬운 교과서 속 어휘25.png";
 import BACKGROUND2 from "@/app/bgpng_temp/6/중등한문_한자를 알면 쉬운 교과서 속 어휘26.png";
+import { ContentContainer } from "@/app/components/content-container";
 
 export default function Page() {
   const [step, setStep] = useState(1);
 
   return (
     <>
-      <ActivityPageTemplate lesson={2}>
-        {step === 1 ? <Content1 /> : null}
-        {step === 2 ? <Content2 /> : null}
+      <ActivityPageTemplate lesson={5}>
+        <Content1 />
       </ActivityPageTemplate>
-      <StepContainer maxStep={2} step={step} onStepChange={setStep} />
       {/* <img src={BACKGROUND2.src} className="debug absolute left-0 top-0 opacity-25 pointer-events-none" /> */}
     </>
   );
@@ -26,9 +24,9 @@ export default function Page() {
 
 const Content1 = () => {
   return (
-    <div className="w-full h-full flex flex-col justify-center items-start -mt-20 -ml-5">
-      <img src={IMAGE1.src} />
-    </div>
+    <ContentContainer className="!justify-start">
+      <img src={IMAGE.src} />
+    </ContentContainer>
   );
 };
 
