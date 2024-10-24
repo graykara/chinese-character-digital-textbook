cf87829b390876dc5855ccd629c24ea7d22816ad
Sunha <sunha@drection.com>
2024-10-24 18:42:00 +0900

    03_026_C1

--- app/(lessons)/lesson2/(3 activities)/(2 다함께생각해요)/03_026_C1/page.tsx
+++ app/(lessons)/lesson2/(3 activities)/(2 다함께생각해요)/03_026_C1/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ThinkTogetherHeader } from "@/app/components/headers/think-together";
+import { ThinkTogetherHeader, ThinkTogetherHeader2 } from "@/app/components/headers/think-together";
 import IMAGE from "./image.png";
 import IMAGE2 from "./image2.png";
 import IMAGE3 from "./image3.png";
@@ -31,14 +31,14 @@ export default function Page() {
 const Step1 = () => {
   return (
     <>
-      <ThinkTogetherHeader title="그림과 기호를 보고 무엇을 의미하는지 말해 보자." />
+      <ThinkTogetherHeader title={<p className="-ml-2">그림과 기호를 보고 무엇을 의미하는지 말해 보자.</p>}></ThinkTogetherHeader>
 
-      <ContentContainer className="absolute left-[40px] bottom-[100px]">
+      <ContentContainer className="absolute left-[40px] bottom-[110px]">
         <img src={IMAGE.src} />
       </ContentContainer>
 
-      <ExampleAnswerButton active={false} onClick={() => {}} />
-      {/* <img src={BACKGROUND1.src} className="absolute left-0 top-0 opacity-30 pointer-events-none" /> */}
+      <ExampleAnswerButton active={false} onClick={() => { }} />
+      <img src={BACKGROUND1.src} className="debug absolute left-0 top-0 opacity-25 pointer-events-none" />
     </>
   );
 };
@@ -48,15 +48,16 @@ const Step2 = () => {
 
   return (
     <>
-      <ThinkTogetherHeader title={
-        <>글자가 없는 세상에 살고 있다고 가정하고 아래의 문장을 간단한 그림이나 기호로 표현해 보자.
-        <BuddyButton className="absolute left-[625px] top-[70px]" />
-        </>} />
+      <ThinkTogetherHeader2
+        title={<p className="-ml-3 tracking-[-1px]">글자가 없는 세상에 살고 있다고 가정하고 아래의 문장을 간단한 그림이나 기호로 표현해 보자.</p>}>
+      </ThinkTogetherHeader2>
+
+      <BuddyButton className="absolute left-[800px] top-[240px]" />
 
       <ContentContainer className="!justify-start mt-20 mb-[100px]">
-        <img src={IMAGE2.src} className="absolute top-[35px] left-[70px]"/>
+        <img src={IMAGE2.src} className="absolute top-[30px] left-[70px]" />
         {showAnswer ? (
-          <img src={IMAGE3.src} className="absolute top-[230px] left-[400px]" />
+          <img src={IMAGE3.src} className="absolute top-[210px] left-[390px]" />
         ) : null}
       </ContentContainer>
 
@@ -64,7 +65,7 @@ const Step2 = () => {
         active={showAnswer}
         onClick={() => setShowAnswer(!showAnswer)}
       />
-      {/* <img src={BACKGROUND2.src} className="absolute left-0 top-0 opacity-30 pointer-events-none" /> */}
+      <img src={BACKGROUND2.src} className="debug absolute left-0 top-0 opacity-25 pointer-events-none" />
     </>
   );
 };
